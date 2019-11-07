1c241e4069bb53f87f5e75662de446cdff2b6c64
marzieh <marziehseyedmoosavi@gmail.com>
2019-11-07 07:11:40 -0800

    :)))))

--- register_page/views.py
+++ register_page/views.py
@@ -12,6 +12,7 @@ def registerUser (request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
+
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
