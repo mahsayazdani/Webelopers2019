9b98548b9e5f77c50c4d7505a8c13600d6d1f7f9
marzieh <marziehseyedmoosavi@gmail.com>
2019-11-07 05:16:44 -0800

    reg edit

--- register_page/views.py
+++ register_page/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from register_page.forms import SignUpForm
+
 
 def registerUser (request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'register_html.html', {'form': form})
 
-    return render(request, 'register_html.html')
+
+
