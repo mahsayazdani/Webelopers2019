3d77bf91cc88ca69fff18cb28d268a1c67a75f39
marzieh <marziehseyedmoosavi@gmail.com>
2019-11-07 07:30:00 -0800

    chnage reg4

--- register_page/views.py
+++ register_page/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from register_page import forms
 from register_page.forms import SignUpForm
 
 
@@ -11,7 +13,12 @@ def registerUser (request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+
+            # if User.objects.filter(username=form.cleaned_data['username']).exists():
+            #     raise forms.ValidationError(u'Username "%s" is already in use.' )
+            # else:
             username = form.cleaned_data.get('username')
+
 
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
@@ -20,6 +27,5 @@ def registerUser (request):
     else:
         form = SignUpForm()
     return render(request, 'register_html.html', {'form': form})
-
 
 
